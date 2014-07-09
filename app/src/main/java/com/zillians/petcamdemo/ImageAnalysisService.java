d8b6bbe8d02e0ba84ecc114d50f5aae9fe91add2
Giggle Liao <giggle@zillians.com>
2014-07-09 12:57:37 +0800

    implement camera preview data to upload to server

---
+++ app/src/main/java/com/zillians/petcamdemo/ImageAnalysisService.java
@@ -0,0 +1,150 @@
+package com.zillians.petcamdemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.SyncHttpClient;
+
+import org.apache.http.Header;
+import org.apache.http.HttpException;
+import org.apache.http.entity.InputStreamEntity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by giggle on 5/6/14.
+ */
+public class ImageAnalysisService {
+    private static final String TAG = "ImageAnalysisService";
+    private static final String IMAGE_ANALYSIS_URL = "http://220.135.58.34:8080/iremember";
+
+    public class Tag {
+        private String tag;
+        private double confidence; // confidence of the tag, 0~1
+
+        public Tag(final String tag, final double confidence) {
+            this.tag = tag;
+            this.confidence = confidence;
+        }
+
+        public String getTag() {
+            return tag;
+        }
+
+        public double getConfidence() {
+            return confidence;
+        }
+    }
+
+    private Bitmap getScaledBitmap(final File image) throws IOException {
+        final double expectDimension = 250;
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(image.getPath(), options);
+
+        // If it's already too small, just return
+        Bitmap bitmap = null;
+        if (options.outWidth < expectDimension || options.outHeight < expectDimension) {
+            bitmap = BitmapFactory.decodeFile(image.getPath());
+        }
+        else {
+            int xRatio = (int) Math.ceil(options.outWidth / expectDimension);
+            int yRatio = (int) Math.ceil(options.outHeight / expectDimension);
+
+            options.inSampleSize = (xRatio > yRatio) ? xRatio : yRatio;
+            options.inJustDecodeBounds = false;
+            bitmap = BitmapFactory.decodeFile(image.getPath(), options);
+        }
+
+        if (bitmap == null) {
+            throw new IOException("Failed to decode bitmap");
+        }
+
+        return bitmap;
+    }
+
+    private InputStream convertBitmapToInputStream(final Bitmap bitmap) {
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+
+        return new ByteArrayInputStream(bos.toByteArray());
+    }
+
+    public Tag[] getTag(final File image) throws IOException, HttpException {
+
+        if (!image.exists()) {
+            throw new FileNotFoundException("File not found: " + image.getPath());
+        }
+
+        class Result {
+            boolean value = true;
+        }
+
+        final List<Tag> myTags = new ArrayList<Tag>();
+        final Bitmap scaledBitmap = getScaledBitmap(image);
+        final InputStream stream = convertBitmapToInputStream(scaledBitmap);
+
+        final Result result = new Result();
+        final AsyncHttpClient client = new SyncHttpClient();
+
+        Log.d(TAG, "Send image to server, size = " + stream.available());
+        client.post(null, IMAGE_ANALYSIS_URL, new InputStreamEntity(stream, stream.available()), "image/jpeg", new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d(TAG, "Success to get tag: " + response.toString());
+
+                final int length = response.length();
+                for (int i = 0; i < length; ++i) {
+                    try {
+                        final JSONArray entry = response.getJSONArray(i);
+                        final double confidence = entry.getDouble(0);
+                        final String tag = entry.getString(1);
+
+                        final Tag result = new Tag(tag, confidence);
+                        myTags.add(result);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                result.value = false;
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                result.value = false;
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                result.value = false;
+            }
+        });
+
+        if (result.value == false) {
+            throw new HttpException("Fail to connect to server");
+        }
+
+        Log.d(TAG, "We retrieve tags count: " + myTags.size());
+        return myTags.toArray(new Tag[0]);
+    }
+}
--- app/src/main/java/com/zillians/petcamdemo/MainActivity.java
+++ app/src/main/java/com/zillians/petcamdemo/MainActivity.java
@@ -1,19 +1,159 @@
 package com.zillians.petcamdemo;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.easycamera.DefaultEasyCamera;
+import com.easycamera.EasyCamera;
+
+import org.apache.http.HttpException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends Activity {
+    private static final String TAG = "PetcamDemo";
+
+    EasyCamera camera;
+    SurfaceView surface;
+    TextView name;
+
+    BlockingQueue<Runnable> queue;
+    ThreadPoolExecutor executor;
+
+    File appFolder;
+    ImageAnalysisService imageAnalysisService = new ImageAnalysisService();
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFolder();
+
+        handler = new Handler();
+        queue = new ArrayBlockingQueue<Runnable>(10);
+        executor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, queue);
+
+        surface = (SurfaceView) findViewById(R.id.preview);
+        name = (TextView) findViewById(R.id.name);
+
+        camera = DefaultEasyCamera.open(1);
+
+        surface.getHolder().addCallback(previewCallback);
     }
 
+    private void initFolder() {
+        appFolder = new File(Environment.getExternalStorageDirectory(), "petcamdemo");
+        if (!appFolder.exists()) {
+            appFolder.mkdir();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        queue.clear();
+        executor.shutdown();
+    }
+
+    private SurfaceHolder.Callback previewCallback = new SurfaceHolder.Callback() {
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.startPreview(holder);
+                camera.setPreviewCallback(cameraPreviewCallback);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            camera.setPreviewCallback(null);
+            camera.close();
+        }
+    };
+
+    private Camera.PreviewCallback cameraPreviewCallback = new Camera.PreviewCallback() {
+
+        private long lastTimeCapture = 0;
+        int captureCount = 0;
+
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            long current = System.currentTimeMillis();
+
+            if (current - lastTimeCapture > 2 * 1000) {
+                lastTimeCapture = current;
+                Log.d(TAG, "Capture data size = " + data.length);
+
+                captureCount++;
+                final File outputImage = new File(appFolder, captureCount + ".jpg");
+                try {
+                    FileOutputStream fos = new FileOutputStream(outputImage);
+                    fos.write(data);
+                    fos.close();
+                    Log.d(TAG, "Write image complete: " + outputImage.toString());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                boolean result = queue.offer(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            final ImageAnalysisService.Tag[] tags = imageAnalysisService.getTag(outputImage);
+                            outputImage.delete();
+                            Log.d(TAG, "Analysis complete: " + tags);
+
+                            handler.post(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    if (tags.length >= 1) {
+                                        name.setText(tags[0].getTag());
+                                    }
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (HttpException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Log.d(TAG, "Put task to upload thread result: " + result);
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
